3a1eee0395e3366a0858c88874d8399f65a64e7b
Jezer Rázuri <jezerrazuri@upeu.edu.pe>
2023-11-07 13:50:39 -0500

    first commit

--- src/pe/com/reales/vista/Index.java
+++ src/pe/com/reales/vista/Index.java
@@ -4,6 +4,8 @@
  */
 package pe.com.reales.vista;
 
+import pe.com.reales.config.bd.ConectaBD;
+import pe.com.reales.config.bd.CrearTablas;
 import pe.com.reales.utils.Lectura;
 import pe.com.reales.utils.Utilitarios;
 
@@ -13,18 +15,25 @@ import pe.com.reales.utils.Utilitarios;
  */
 public class Index {
     private static Lectura leer=new Lectura();
-    public static void menuprincipal(){
+    public static void menuPrincipal(){
         System.out.println("= MENU PRINCIPAL =");
         System.out.println("1. Editorial");
         System.out.println("2. Lector");
         System.out.println("3. Texto");
-        System.out.println("4. Salir");
+        System.out.println("4. Reporte");
+        System.out.println("5. Salir");
         System.out.print("Elija una de las opciones: ");
     }
-    public static void inicioprincipal(){
+    public static void menuCreacion(){
+        System.out.println("= MENU PRINCIPAL =");
+        System.out.println("1. Crear Tablas");
+        System.out.println("2. Salir");
+        System.out.print("Elija una de las opciones: ");
+    }
+    public static void inicioMenuPrincipal(){
         int opcion;
         do{
-            menuprincipal();
+            menuPrincipal();
             opcion=leer.entero();
             switch (opcion){
                 case 1 -> IndexEditorial.inicio();
@@ -32,9 +41,25 @@ public class Index {
                 case 3 -> IndexTexto.inicio();
                 case 4 -> Utilitarios.salir();
             }
+        } while(opcion!=5);
+    }
+    public static void inicioMenuCreacion(){
+        int opcion;
+        do{
+            menuPrincipal();
+            opcion = leer.entero();
+            switch (opcion){
+                case 1 -> IndexEditorial.inicio();
+                case 2 -> Utilitarios.salir();
+            }
         } while(opcion!=2);
     }
+    public static void mostrarInicio(){
+        System.out.println(CrearTablas.countTables());
+        inicioMenuPrincipal();
+    }
+
     public static void main(String[] args) {
-        inicioprincipal();
+        mostrarInicio();
     }
 }
